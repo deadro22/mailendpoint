a608ef209964471d7c14a4dff7721d17ead74c6c
unknown <gamemangamer8@gmail.com>
2022-04-14 12:08:33 +0100

    rf

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    auth: {
+      user: process.env.USR,
+      pass: process.env.PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: `<${req.body.mail}>`,
+    to: process.env.USR,
+    subject: req.body.subject,
+    html: `<h4>From:</h4>${req.body.mail}\n<h4>Message:</h4><p style="font-weight:600">${req.body.message}</p>`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      res.end("error");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.end("Sent");
+    }
+  });
+});
+
+app.listen(process.env.PORT || 80);
